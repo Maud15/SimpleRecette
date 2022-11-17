2aa684d719854da0b93b957d9db15b35e78ebff5
Maud <maud.chevalier15@gmail.com>
2022-11-17 18:18:08 +0100

    Add getters and setters for E_Recette and E_Ingredient

--- src/main/java/com/jpa/example/model/E_Ingredient.java
+++ src/main/java/com/jpa/example/model/E_Ingredient.java
@@ -24,8 +24,28 @@ public class E_Ingredient {
     )
     private List<E_Recette> recettes;
 
-
     public E_Ingredient() {
+    }
+
+    public Long getIngredientId() {
+        return ingredientId;
+    }
+    public void setIngredientId(Long ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public List<E_Recette> getRecettes() {
+        return recettes;
+    }
+    public void setRecettes(List<E_Recette> recettes) {
+        this.recettes = recettes;
     }
 
 }
